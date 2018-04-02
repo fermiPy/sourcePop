48f4b121a56778bf319a6d4b8b2c40a47e2ad741
Eric Charles <echarles@gmail.com>
2018-04-02 13:41:14 -0700

    added skeleton classes

---
+++ sourcePop/src_dist.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import, division, print_function
+
+"""
+This is a generic file-level docstring
+"""
+
+
+
